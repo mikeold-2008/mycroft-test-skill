4eaeb9fc67d0cfd59d0d0d413b0e6989d296901d
Michael Oldman <mikeold2008@gmail.com>
2022-11-10 14:42:53 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/lightbulb.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/> Mycroft Test
+Just me testing mycroft
+
+## About
+
+
+## Examples
+* "Test"
+
+## Credits
+mikeold-2008
+
+## Category
+**Information**
+
+## Tags
+
---
+++ __init__.py
@@ -0,0 +1,19 @@
+from mycroft import MycroftSkill, intent_file_handler
+
+
+class MycroftTest(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+
+    @intent_file_handler('test.mycroft.intent')
+    def handle_test_mycroft(self, message):
+        intensity = ''
+
+        self.speak_dialog('test.mycroft', data={
+            'intensity': intensity
+        })
+
+
+def create_skill():
+    return MycroftTest()
+
